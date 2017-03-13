ebc68273206c3cdb67242e96d92d0f99a8a8dde4
Bruno Ricardo Siqueira <brunoric@gmail.com>
2017-03-13 23:13:51 +0000

    Adding build test

---
+++ test/all-tests.sh
@@ -0,0 +1,14 @@
+PERCONA_TOOLKIT_VERSION=3.0.1;
+
+function version_test()
+{
+    local VERSION=$(docker run --rm -it brunoric/percona-toolkit:3.0.1 pt-query-digest --version)
+    if [[ $VERSION == *"${PERCONA_TOOLKIT_VERSION}"* ]]; then
+        echo 'Version test OK!'; 
+    else
+        echo 'Version test Failed!'; 
+        exit 1;
+    fi
+}
+
+version_test;
